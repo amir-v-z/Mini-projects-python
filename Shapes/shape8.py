bb7d2ecfa854fcd5bc06272f32eca870274adc61
amir_v_z <amirhosseinvakilzadeh82@gmail.com>
2024-04-16 20:57:45 +0330

    Create shape8.py

---
+++ Shapes/shape8.py
@@ -0,0 +1,24 @@
+size = 15
+
+for a in range(int(size / 2), size + 1, 2):
+    for b in range(1, size - a, 2):
+        print(" ", end= "")
+
+    for b in range(1, a + 1):
+        print("#", end= "")
+
+    for b in range(1, (size - a) + 1):
+        print(" ", end= "")
+
+    for b in range(1, a):
+        print("#", end= "")
+
+    print("")
+
+for a in range(size, -1, -1):
+    for b in range(a, size):
+        print(" ", end= "")
+
+    for b in range(1, (a * 2)):
+        print("#", end= "")
+    print("")
